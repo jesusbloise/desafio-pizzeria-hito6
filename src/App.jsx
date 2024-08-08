4245cdd69b71a75451e5441a29c3b27e2c449e23
jesus bloise <jesusbloise@gmail.com>
2024-08-07 20:48:09 -0400

    haciendo los formularios de register y login

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MyNavbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Register from './components/Register';
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
       <div>
         <MyNavbar />
-        <Routes>
+        <div className="App">
+          <h1 class="text-center">Formulario de Registro</h1>
+          <Register />
+          <h1 class="text-center">Formulario de Login.</h1>
+          <Login />
+        </div>
+      
+        {/* <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-        <Footer />
+        <Footer /> */}
       </div>
     </Router>
   );
